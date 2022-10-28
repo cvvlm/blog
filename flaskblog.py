b306f9befaa41b437531610102c089190f9d3dcb
cvvlm <michalsk7@gmail.com>
2022-10-28 09:30:54 +0200

    Do layout page and bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Mateusz Michalski',
+        'title': 'Blog post1', 
+        'content': 'First post content',
+        'date_posted': '27.10.2022r.'
+    },
+    {
+        'author':'random_nickname',
+        'title': 'Blog post2', 
+        'content': 'Second post content',
+        'date_posted': '27.10.2022r.'
+    }
+
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Home Page</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="about")
 
 
 if __name__ == '__main__':
